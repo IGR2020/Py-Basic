5c5061695c0cf043ffa07aecd3b3775841564e62
IGR2020 <159914009+IGR2020@users.noreply.github.com>
2024-04-29 16:03:43 +0530

    Variable tracking.

--- compiler.py
+++ compiler.py
@@ -23,42 +23,93 @@ del temp
 # opening output file and preparing
 file = open("output.py", "w")
 file.write(
-    "import pygame as pg\nimport EPT\n\nassets={}\n\nwindow_width, window_height = 900, 500\nwindow=pg.display.set_mode((window_width, window_height))\nbackground = (255, 255, 255)\n\nclock = pg.time.Clock()\nfps=60\nrun = True\n\n"
+    "import pygame as pg\nimport EPT\n\nassets={}\n\nwindow_width, window_height = 900, 500\nwindow=pg.display.set_mode((window_width, window_height))\nbackground = (255, 255, 255)\n\nclock = pg.time.Clock()\nfps=60\nrun = True\n\ntext_colour=(0, 0, 0)\n\n"
 )
 file.close()
 file = open("output.py", "a")
 
+vars_defined = []
 skip_counter = 0
 
 for line, indent in zip(code, indent_map):
+
     for i, word in enumerate(line):
+
         if skip_counter > 0:
             skip_counter -= 1
             continue
+
         elif word == "load":
+
             if len(line) > 2 and line[-3] == "size":
                 file.write(indent+
                     f"assets.update(EPT.load_assets('{line[i+1]}', ({line[-2]}, {line[-1]})))\n"
                 )
+
             else:
                 file.write(indent+f"assets.update(EPT.load_assets('{line[i+1]}'))\n")
+
             skip_counter = len(line)
+
         elif word == "show":
-            file.write(indent+
+
+            if "." in line[i+1]:
+                file.write(indent+
                 f"window.blit(assets['{line[i+1]}'], ({line[-2]}, {line[-1]}))\n"
             )
+                
+            else:
+
+                if " ".join(line[i+1:-3]) in vars_defined:
+                    file.write(indent+
+                    f"EPT.blit_text(window, {" ".join(line[i+1:-3])}, pos=({line[-2]}, {line[-1]}), colour=text_colour)\n"
+                )
+
+                else:
+                    file.write(indent+
+                        f"EPT.blit_text(window, '{" ".join(line[i+1:-3])}', pos=({line[-2]}, {line[-1]}), colour=text_colour)\n"
+                    )
+
             skip_counter = len(line)
-        elif word in ("while", "for", "if"):
+
+        elif " ".join(line[:2]) == "text colour":
+
+            file.write(indent + f"text_colour {" ".join(line[2:])}\n")
+            skip_counter = len(line)
+
+        elif " ".join(line[:2]) == "window name":
+
+            if " ".join(line[3:]) in vars_defined:
+                file.write(indent + f"pg.display.set_caption({" ".join(line[3:])})\n")
+
+            else:
+                file.write(indent + f"pg.display.set_caption('{" ".join(line[3:])}')\n")
+            skip_counter = len(line)
+
+
+        elif word in ("while", "for", "if", "def"):
             file.write(indent+f"{word} {" ".join(line[1:])}:\n")
+
             if " ".join(line) == "while run":
-                file.write(f"{indent}    for event in pg.event.get():\n{indent}        if event.type == pg.QUIT:\n{indent}            run = False\n")
+
+                file.write(f"{indent}    clock.tick(fps)\n{indent}    for event in pg.event.get():\n{indent}        if event.type == pg.QUIT:\n{indent}            run = False\n")
+            
             skip_counter = len(line)
+
         elif word == "end":
             file.write("pg.quit()\nquit()\n")
             skip_counter = len(line)
+
         elif word == "refresh":
             file.write(f"{indent}pg.display.update()\n{indent}window.fill(background)\n")
+
         else:
+
             file.write(indent+" ".join(line) + "\n")
+
+            if "=" in line:
+                vars_defined.append(word)
+
             skip_counter = len(line)
+
     skip_counter = 0
--- output.py
+++ output.py
@@ -11,13 +11,21 @@ clock = pg.time.Clock()
 fps=60
 run = True
 
+text_colour=(0, 0, 0)
+
 assets.update(EPT.load_assets('assets'))
+name = "Bob"
+pg.display.set_caption(name)
 while run:
+    clock.tick(fps)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
     window.blit(assets['smile.jpeg'], (100, 100))
     pg.display.update()
     window.fill(background)
-pg.quit
+    text_colour = (90, 10, 30)
+    EPT.blit_text(window, name, pos=(400, 200), colour=text_colour)
+    EPT.blit_text(window, 'is this good', pos=(400, 250), colour=text_colour)
+pg.quit()
 quit()
